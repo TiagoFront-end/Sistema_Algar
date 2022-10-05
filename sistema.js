17222da64f5a45ad345c2d45f4338bc6a670c8a2
Tiago Teixeira da Silva - PRESTADOR - ALGAR 2 <08866579971@prestador.tjdft.jus.br>
2022-10-05 17:09:46 -0300

    sistema

---
+++ sistema.js
@@ -0,0 +1,16 @@
+
+function menuOpen(){
+document.querySelector(".menu-lateral").style.width="200px";
+document.querySelector(".nav-item").style.marginLeft="100px";
+
+
+
+}
+
+
+function menuClose(){
+    document.querySelector(".menu-lateral").style.width="0px";
+    document.querySelector(".nav-item").style.marginLeft="0px";
+    
+}
+
